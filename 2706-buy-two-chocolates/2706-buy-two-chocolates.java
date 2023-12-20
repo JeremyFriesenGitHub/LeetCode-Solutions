092cc1f9836982d97c7c5f5ff8d54c5d094af224
Jeremy Friesen <123791412+JeremyFriesenGitHub@users.noreply.github.com>
2023-12-19 19:37:11 -0500

    Time: 24 ms (5.97%), Space: 41.8 MB (100.00%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int savedIndex=-1;
+
+        for (int i=0;i<prices.length;i++){
+            if (prices[i]<min1){
+                min1=prices[i];
+                savedIndex=i;
+                i=0;  
+            }
+            if(min2>prices[i] && prices[i]>=min1 && savedIndex!=i){
+                min2=prices[i];
+            }   
+        }
+
+        int newSum = money - min1 -min2;
+        if (newSum >=0){
+            System.gc();
+            return newSum;
+        }
+        System.gc();
+        return money;
+    }
+}
