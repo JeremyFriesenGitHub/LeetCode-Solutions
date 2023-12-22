0e44160e7a6533253c93bc0c9f306dffa90b8d80
Jeremy Friesen <123791412+JeremyFriesenGitHub@users.noreply.github.com>
2023-12-22 00:09:47 -0500

    Time: 109 ms (10.72%), Space: 17.6 MB (5.69%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -6,33 +6,48 @@ class Solution:
         left=[0]*length
         right =[0]*length
         maxNums=[0]*length
-
+        
+        #set left and right arrays
         for i in range(length):
             if(i>0):
+                
                 left[i]= left[i-1] + s[i]
                 right[i]= s[i+1:]
 
             else:
+                
                 left[i]=s[i]
                 right[i]=s[i + 1:]
                 
+        #count the ones and zeros in left and right array
         for j in range(len(left)):
+            
             stringLeft=left[j]
             stringRight=right[j]
             countZeros=0
             countOnes=0
+            
             for k in range(len(stringLeft)):
+                
                 if (stringLeft[k]=="0"):
                     countZeros=countZeros+1
+                    
             for l in range(len(stringRight)):
+                
                 if (stringRight[l]=="1"):
                     countOnes=countOnes+1
+            
+            #add both counts to the max array
             maxNums[j]=countZeros+countOnes
         
+        
+        #max 
         max=-1
+        
+        #get max 
         for m in range(len(maxNums)):
+            
             if (max<maxNums[m]):
                 max=maxNums[m]
-        print(max)
-
+        #return
         return max
