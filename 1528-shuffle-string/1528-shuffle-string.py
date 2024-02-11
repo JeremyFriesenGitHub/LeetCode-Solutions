3e54b66a944b24febb7ec25309907f747f3900a6
Jeremy Friesen <jeremyfriesen12@gmail.com>
2024-02-11 10:14:27 -0500

    Time: 62 ms (24.09%), Space: 16.5 MB (69.32%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,5 +1,6 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
+        #
         newStr=[0]*len(indices)
         for i in range(len(indices)):
             newStr[indices[i]]= s[i]
