aa823bc752be86a582852b2d5759871d40abc314
Jeremy Friesen <jeremyfriesen12@gmail.com>
2024-03-08 19:36:18 -0500

    Time: 346 ms (69.30%), Space: 42.7 MB (14.17%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -3,4 +3,4 @@ class Solution:
         set1=set(nums1)
         set2=set(nums2)
         
-        return min(set1 & set2) if set1 & set2 else -1
+        return min(set1.intersection(set2)) if set1.intersection(set2) else -1
