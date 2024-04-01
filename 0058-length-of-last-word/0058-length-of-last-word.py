204518c609047e425cf27010c12f3678e2ea29f9
Jeremy Friesen <jeremyfriesen12@gmail.com>
2024-03-31 22:18:19 -0400

    Time: 23 ms (98.40%), Space: 16.6 MB (82.28%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,12 +1,7 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        # Split the input string by space and filter out empty strings
-        words = s.split()
-        
-        # Check if there are any words in the list
-        if words:
-            # Return the length of the last word in the list
-            return len(words[-1])
-        else:
-            # If no words found, return 0
-            return 0
+        string = list(s.split(" "))
+        filteredString = [string for string in string if string]
+        print(filteredString)
+        lastWord = filteredString[len(filteredString) - 1]
+        return len(lastWord)
